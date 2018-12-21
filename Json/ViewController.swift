280c71436bd56fc21701c0b43f0382fcabddb943
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-12-20 23:00:29 -0600

    commit final

--- Json/ViewController.swift
+++ Json/ViewController.swift
@@ -1,25 +1,58 @@
-//
-//  ViewController.swift
-//  Json
-//
-//  Created by Freddy on 31/12/00.
-//  Copyright © 2000 Freddy. All rights reserved.
-//
-
+import WebKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var salida: UITextField!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var wkWebView: WKWebView!
+    var definicion:String?
+    var palabra:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buscar(_ sender: Any) {
+        definicion = textField.text!
+        let urlWiki = "https://es.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=\(definicion!.replacingOccurrences(of: " ", with: "%20"))"
+        let urlObjeto = URL(string:urlWiki)
+        let tarea = URLSession.shared.dataTask(with: urlObjeto!) { (datos, respuesta, error) in
+            if error == nil {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: datos!, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:Any]
+                    // print(json)
+                    
+                    let querySubJson = json["query"] as! [String:Any]
+                    
+                    //print(querySubJson)
+                    let pagesSubJson = querySubJson["pages"] as! [String:Any]
+                    //print(pagesSubJson)
+                    
+                    let keySubJson = pagesSubJson.keys.first
+                    
+                    let idSubJson = pagesSubJson[keySubJson!] as! [String:Any]
+                    
+                    //print(idSubJson)
+                    
+                    
+                    let extractStringHtml = idSubJson["extract"] as! String
+                    self.palabra = extractStringHtml
+                    
+                    //print(extractStringHtml)
+                    
+                    DispatchQueue.main.sync(execute: {
+                        
+                        self.wkWebView.loadHTMLString(extractStringHtml, baseURL: nil)
+                    })
+                }catch {
+                    print("El Procesamiento del JSON tuvo un error")
+                }
+            } else {
+                print(error!)
+            }
+        }
+        self.salida.text = self.palabra
+        tarea.resume()
     }
-
-
 }
-
